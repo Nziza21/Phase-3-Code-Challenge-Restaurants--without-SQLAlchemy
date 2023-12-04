f4a03307b170e2e917cdb0dd858ad7338a3e490f
Nziza Samuel <nziza.samuel@student.moringaschool.com>
2023-12-04 11:25:19 +0300

    completed

---
+++ restaurant.py
@@ -0,0 +1,23 @@
+from review import Review
+
+class Restaurant:
+    all_restaurants = []
+
+    def __init__(self, name):
+        self._name = name
+        Restaurant.all_restaurants.append(self)
+
+    def name(self):
+        return self._name
+
+    def reviews(self):
+        return [review for review in Review.all_reviews() if review.restaurant() == self]
+
+    def customers(self):
+        return list(set(review.customer() for review in self.reviews()))
+
+    def average_star_rating(self):
+        ratings = [review.rating() for review in self.reviews()]
+        if not ratings:
+            return 0
+        return sum(ratings) / len(ratings)
