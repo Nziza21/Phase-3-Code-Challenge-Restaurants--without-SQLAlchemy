814e38377f892600080577046ac1ea8bc703ae4d
Nziza Samuel <nziza.samuel@student.moringaschool.com>
2023-12-04 11:24:34 +0300

    completed

--- review.py
+++ review.py
@@ -1,1 +1,21 @@
+class Review:
+    all_reviews = []
 
+    def __init__(self, customer, restaurant, rating):
+        self._customer = customer
+        self._restaurant = restaurant
+        self._rating = rating
+        Review.all_reviews.append(self)
+
+    def rating(self):
+        return self._rating
+
+    @classmethod
+    def all_reviews(cls):
+        return cls.all_reviews
+
+    def customer(self):
+        return self._customer
+
+    def restaurant(self):
+        return self._restaurant
