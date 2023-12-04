fe835c9b6c1e1587a6b227090f4a7d01b079f040
Nziza Samuel <nziza.samuel@student.moringaschool.com>
2023-12-04 11:11:38 +0300

    completed

---
+++ customer.py
@@ -0,0 +1,45 @@
+from review import Review
+
+class Customer:
+    all_customers = []
+
+    def __init__(self, given_name, family_name):
+        self._given_name = given_name
+        self._family_name = family_name
+        Customer.all_customers.append(self)
+        self.reviews = []
+
+    def given_name(self):
+        return self._given_name
+
+    def family_name(self):
+        return self._family_name
+
+    def full_name(self):
+        return f"{self._given_name} {self._family_name}"
+
+    @classmethod
+    def all(cls):
+        return cls.all_customers
+
+    def restaurants(self):
+        return list(set(review.restaurant() for review in self.reviews))
+
+    def add_review(self, restaurant, rating):
+        new_review = Review(self, restaurant, rating)
+        self.reviews.append(new_review)
+        return new_review
+
+    def num_reviews(self):
+        return len(self.reviews)
+
+    @classmethod
+    def find_by_name(cls, name):
+        for customer in cls.all_customers:
+            if customer.full_name() == name:
+                return customer
+        return None
+
+    @classmethod
+    def find_all_by_given_name(cls, name):
+        return [customer for customer in cls.all_customers if customer.given_name() == name]
---
+++ main.py
@@ -0,0 +1,24 @@
+from customer import Customer
+from restaurant import Restaurant
+from review import Review
+
+
+from customer import Customer
+from restaurant import Restaurant
+from review import Review
+
+# Sample instances for testing
+customer1 = Customer("George", "Washington")
+customer2 = Customer("John", "Adams")
+restaurant1 = Restaurant("The Best Restaurant")
+restaurant2 = Restaurant("Delicious Eats")
+
+# Testing methods
+review1 = customer1.add_review(restaurant1, 4)
+review2 = customer2.add_review(restaurant1, 5)
+review3 = customer1.add_review(restaurant2, 3)
+
+print(customer1.full_name())  # Output: George Washington
+print(customer1.restaurants())  # Output: [The Best Restaurant, Delicious Eats]
+print(restaurant1.customers())  # Output: [George Washington, John Adams]
+print(restaurant1.average_star_rating())  # Output: 4.5
